dfed5842124b81563dae11aa281e312a16e7598e
VasylFId <157602750+VasylFId@users.noreply.github.com>
2024-04-22 20:31:36 +0100

    Update count.py

--- count.py
+++ count.py
@@ -1,1 +1,21 @@
-
+a = True
+b = 0
+c  = [5]
+l = len(c)
+while a:
+    if b%2 == 0 or b%3 == 0 :
+       b=b
+    else :
+            e = 0
+            for i in c:
+                if(b%i>0 and b!=1):
+                    e=e+1
+            if e ==  len(c) :
+                print(b)
+                c.append(b)
+    b=b+1
+    
+    print("count" , b)
+    for y in c:
+        print(y)
+         
